3ec58202dbc33ae24c29511586b16dee6fa7d854
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-04-14 20:46:02 +0530

    Update and rename read arrays.c to read 1d arrays.c

--- read 1d arrays.c
+++ read 1d arrays.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main() {
-   int marks[4];
+   int arr[4];
    
    for(int i=0; i<4; i++){
        printf("Enter the value of %d element in the array\n",i);
-       scanf("%d",&marks[i]);
+       scanf("%d",&arr[i]);
    }
    for(int i=0; i<4; i++)
-           printf("the value of %d element of the array is %d\n",i,marks[i]);
+           printf("the value of %d element of the array is %d\n",i,arr[i]);
    }
     
    
